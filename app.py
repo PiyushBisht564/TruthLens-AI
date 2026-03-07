fb7dd5e575e27b6e7ef67f10d2a1366af4af6db5
Piyush Bisht <piyushbisht564@gmail.com>
2026-03-07 16:41:59 +0530

    updating app.py for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from model import predict_image
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
